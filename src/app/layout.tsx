f359e30244a99bb185a87c54bba6554edb34f9ab
Usman Butt <usman.butt@outlook.com>
2024-02-02 16:15:50 -0500

    Add SpeedInsights component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -21,6 +22,7 @@ export default function RootLayout({
         {children}
         <footer className='text-blue-600'>Copyright Usman Inc.</footer>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
