46cc9c86379080c5c2f856cf178fdd14a11656d5
Usman Butt <usman.butt@outlook.com>
2024-02-02 16:11:21 -0500

    Add Analytics component to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <footer className='text-blue-600'>Copyright Usman Inc.</footer>
+        <Analytics />
       </body>
     </html>
   );
